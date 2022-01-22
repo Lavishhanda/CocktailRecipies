e4c778735c3729c59930c4f9586b357da5b16212
Lavishhanda <lavish.handa@gmail.com>
2022-01-22 16:11:03 -0500

    logo css fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import logo from "../logo.svg";
+import logo1 from "../logo1.svg";
 
 export default function Navbar() {
   return (
   <nav className="navbar">
     <div className="nav-center">
-      <img src={logo} alt="img" className="logo"></img>
+      <img src={logo1} alt="img" className="logo"></img>
       <ul className="nav-links">
         <li>
           <Link to="/">Home</Link>
